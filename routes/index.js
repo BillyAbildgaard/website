36492e59d30847d9788840d20602770fa59892cf
Billy Abildgaard <william.abildgaard@gmail.com>
2017-11-03 13:53:50 -0400

    HTML template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Billy's website"  });
+  //res.render('index', { title: "Billy's website"  });
+
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 module.exports = router;
